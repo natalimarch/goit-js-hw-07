5575579c1fa8fdee5755f17d246805fcc8c6c77d
Nataliia Pogrebna <natalibunny13@gmail.com>
2021-08-31 15:31:04 +0300

    task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -12,9 +12,11 @@ function createImgCard(galleryItems) {
 return galleryItems
     .map(({preview, original, description}) => {
         return `
+    <li>
     <a class="gallery__item" href="${original}">
     <img class="gallery__image" src="${preview}" alt="${description}" />
     </a>
+    </li>
     `}).join('');   
 }
 
