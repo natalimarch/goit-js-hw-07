eb71d58283554fa89019470353730080b10f324d
Nataliia Pogrebna <natalibunny13@gmail.com>
2021-08-31 13:45:10 +0300

    task01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,69 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+const galleryItem = createImgCard(galleryItems);
+
+gallery.insertAdjacentHTML("beforeend", galleryItem);
+
+function createImgCard(galleryItems) {
+return galleryItems
+    .map(({preview, original, description}) => {
+        return `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+    </div>
+    `}).join('');   
+}
+
+gallery.addEventListener("click", choosePic);
+
+function choosePic(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+  return;
+  }
+
+  const modalBtn = {
+    onShow: instance => {
+      document.addEventListener('keydown', escBtn);
+    },
+    onClose: instance => {
+      document.removeEventListener('keydown', escBtn);
+    },
+    // onShow: instance => {
+    //   document.addEventListener('keydown', enterBtn);
+    // },
+    // onClose: instance => {
+    //   document.removeEventListener('keydown', enterBtn);
+    // }
+}
+
+const instance = basicLightbox.create(`<img src="${event.target.dataset.source}">`, modalBtn);
+
+
+instance.show()
+
+function escBtn(event) {
+  if (event.key === 'Escape') {
+    instance.close();
+  }
+}
+
+// function enterBtn(event) {
+//   if (event.key === 'Enter') {
+//     instance.open();
+//   }
+// }
+}
+
+
